72e2346a8f67cf8425b8b8e21928e229438ebf32
m_utkej <m_utkej@utke2.site>
2008-10-27 17:24:16 -0500

    use scmStatus

--- Manual/createVersionInfo.sh
+++ Manual/createVersionInfo.sh
@@ -2,10 +2,9 @@
 hg tip > /dev/null
 if [ $? -ne 0 ] 
 then 
-  echo "hg: n/a by $USER" > versionInfo.txt.new
+  echo "vers. unknown" > versionInfo.txt.new
 else
-  echo -n "$(hg tip | grep changeset: | sed 's/changeset:[ ]*\(.*\):\(.*\)/hg:\1/'):$(hg id | sed 's/\([^ ].*\) \(.*\)/\1/')" > versionInfo.txt.new
-  echo " by $(echo $USER | sed 's/_/\\_/')" >> versionInfo.txt.new  
+  echo -n "vers. hg:$(hg id -i):$(hg id  -n)" > versionInfo.txt.new
 fi
 diff versionInfo.txt.new versionInfo.txt > /dev/null
 if [ $? -ne 0 ]
@@ -13,4 +12,25 @@ then
   mv versionInfo.txt.new versionInfo.txt
 else 
   rm  versionInfo.txt.new
+fi
+
+THISDIR=$PWD
+cd $OPENADROOT
+if [ -f bin/scmStatus ]
+then 
+  scmStatus -l >  $THISDIR/versionInfoOpenAD.txt.new
+  if [ $? -ne 0 ] 
+  then 
+    echo "vers. unknown" > $THISDIR/versionInfoOpenAD.txt.new
+  fi
+else
+  echo "vers. unknown" > $THISDIR/versionInfoOpenAD.txt.new
+fi 
+cd $THISDIR
+diff versionInfoOpenAD.txt.new versionInfoOpenAD.txt > /dev/null
+if [ $? -ne 0 ]
+then 
+  mv versionInfoOpenAD.txt.new versionInfoOpenAD.txt
+else 
+  rm  versionInfoOpenAD.txt.new
 fi

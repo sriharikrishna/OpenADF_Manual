f6aaddabafac303910b6a9ab9e9ed3fe593cdedf
m_utkej <m_utkej@utke2.site>
2009-02-17 14:25:24 -0600

    options

--- Manual/fold.py
+++ Manual/fold.py
@@ -4,8 +4,18 @@
 import sys,string
 
 def main():
+  from optparse import OptionParser
+  usage = '%prog [options]\n\t fold long lines'
+  opt = OptionParser(usage=usage)
+  opt.add_option('-w','--width',dest='width',
+                 help="width to be respected (default 132)",
+                 type="int",default=132)
+  opt.add_option('-c','--continuation',dest='continuation',
+                 help="use \'\\\' as continuation character",
+                 action='store_true',default=False)
+  (options, args) = opt.parse_args()
   for line in sys.stdin.readlines():
-    if len(line)>132:
+    if len(line)>options.width:
        words = string.split(line)
        cWord=0
        sWord=0
@@ -14,16 +24,18 @@ def main():
                sys.stdout.write(words[cWord]+"\n")
                break
            tl=0
-           while tl<=132 and cWord<len(words)-1:
+           while tl<=options.width and cWord<len(words)-1:
                tl+=len(words[cWord])
                cWord+=1
-           if (tl>132 and cWord>sWord+1):
+           if (tl>options.width and cWord>sWord+1):
               cWord-=1
            for i in range(sWord,cWord):
                sys.stdout.write(words[i])
                sys.stdout.write(' ')
            if cWord<len(words):
-               sys.stdout.write("\\\n\t")
+             if (options.continuation):
+               sys.stdout.write("\\")
+             sys.stdout.write("\n\t")
            sWord=cWord
     else:
         sys.stdout.write(line)

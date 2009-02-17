ff50a560ec02e1fb387f0d05c9fef40a33261e59
m_utkej <m_utkej@utke2.site>
2009-02-17 17:10:48 -0600

    more fixes

--- Manual/createOpenadOptionsAndOutput.sh
+++ Manual/createOpenadOptionsAndOutput.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -u
-${OPENADROOT}/bin/openad -h > openadScriptOptions.txt
+${OPENADROOT}/bin/openad -h | ./fold.py -w 80 > openadScriptOptions.txt
 echo "        subroutine head(x)
         end subroutine" > head.f
 ${OPENADROOT}/bin/openad head.f > openadScriptOutput.txt
-${OPENADROOT}/bin/openad -n head.f | ./fold.py > openadScriptNoAction.txt
+${OPENADROOT}/bin/openad -n head.f | ./fold.py -c > openadScriptNoAction.txt

a133f508211403e5c7065ac25a223ec2a3fb4356
m_utkej <m_utkej@utke2.site>
2009-04-10 13:21:59 -0500

    nonverbose

--- Manual/getBib.sh
+++ Manual/getBib.sh
@@ -4,6 +4,6 @@ then
   echo "cannot reach autodiff.org, using old file" 
   exit 0
 fi
-wget -O $1.gz http://www.autodiff.org/?module=Publications\&submenu=download%20all%20entries
+wget -nv -O $1.gz http://www.autodiff.org/?module=Publications\&submenu=download%20all%20entries
 gunzip -f $1.gz
 dos2unix $1

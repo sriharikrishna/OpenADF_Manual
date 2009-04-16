538f3f32b418825a330525b1a219e238cb9e0682
m_utkej <m_utkej@utke2.site>
2009-04-16 17:51:08 -0500

    more options figures/text

--- Manual/code/daerfj.f90
+++
@@ -1,9 +0,0 @@
-subroutine daerfj(x,fvec)
-  double precision x(4), fvec(11)
-  ! declare and initialize local variables
-  do i = 1, 11
-    ! more computations
-    fvec(i) = y(i) x(1)*t emp1/temp2
-  end do
-  ! print some results (PASSIVE statements)
-end subroutine daerfj

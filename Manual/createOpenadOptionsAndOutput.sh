9433920643aef975f99732032dc8fb35973f55d5
m_utkej <m_utkej@utke2.site>
2010-02-27 07:40:59 -0600

    force exit with return code and make output wider

--- Manual/createOpenadOptionsAndOutput.sh
+++ Manual/createOpenadOptionsAndOutput.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -u
+set -e
 ${OPENADROOT}/bin/openad -h | ./fold.py -w 80 > openadScriptOptions.txt
 echo "        subroutine head(x)
         end subroutine" > head.f
 ${OPENADROOT}/bin/openad head.f > openadScriptOutput.txt
-${OPENADROOT}/bin/openad -n head.f | ./fold.py -c > openadScriptNoAction.txt
+${OPENADROOT}/bin/openad -n head.f | ./fold.py -c -w 136 > openadScriptNoAction.txt

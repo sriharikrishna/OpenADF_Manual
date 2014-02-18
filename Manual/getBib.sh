f4c878a9241bb7946ac5cf89a73f80079c8e472f
utke on macbook <utke on macbook@localhost>
2014-02-18 10:14:39 -0600

    script updated

--- Manual/getBib.sh
+++ Manual/getBib.sh
@@ -4,6 +4,5 @@ then
   echo "cannot reach autodiff.org, using old file" 
   exit 0
 fi
-wget -nv -O $1.gz http://www.autodiff.org/?module=Publications\&submenu=download%20all%20entries
-gunzip -f $1.gz
+wget -nv -O $1 http://www.autodiff.org/?module=Publications\&submenu=download%20all%20entries
 dos2unix $1
